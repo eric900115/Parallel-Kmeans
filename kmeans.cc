ad1da136b96ae8e349be92cc03996f392ebba589
eric900115 <eric20607@gmail.com>
2023-12-29 20:19:36 +0800

    feat : kmeans seqeuntial version 1

--- kmeans.cc
+++ kmeans.cc
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <zlib.h>
 #include <png.h>
+#include <math.h>
 
 unsigned char* image_src, *image_result;
 
@@ -77,6 +78,123 @@ void write_png(const char* filename, png_bytep image, const unsigned height, con
     fclose(fp);
 }
 
+void kmeans(unsigned char* image_src, unsigned char* image_result, unsigned height, unsigned width, unsigned channels, unsigned num_cluster) {
+
+    int *pt_cluster = (int*) malloc(height * width * sizeof(int));
+    char *centroid = (char*) malloc(channels * num_cluster * sizeof(char));
+    char *new_centroid = (char*) malloc(channels * num_cluster * sizeof(char));
+    int *sum_dist = (int*) malloc(channels * num_cluster * sizeof(int));
+    int *num_pt_cluster = (int*) malloc(num_cluster * sizeof(int));
+    char val[3];
+    int dist, min_dist, idx, sum_val;
+
+    // get random center
+    for(int i = 0; i < num_cluster; i++) {
+        centroid[channels * i + 0] = image_src[channels * (i * 20 + 15 * i * width) + 0];
+        centroid[channels * i + 1] = image_src[channels * (i * 20 + 15 * i * width) + 1];
+        centroid[channels * i + 2] = image_src[channels * (i * 20 + 15 * i * width) + 2];
+    }
+
+    while (1)
+    {
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                
+                val[0] = image_src[channels * (j + i * width) + 0];
+                val[1] = image_src[channels * (j + i * width) + 1];
+                val[2] = image_src[channels * (j + i * width) + 2];
+                min_dist = 1000000;
+                
+                for(int k = 0; k < num_cluster; k++) {
+                    // calculate l2 norm
+                    dist = 0;
+                    dist += (val[0] - centroid[channels * k + 0]) * (val[0] - centroid[channels * k + 0]);
+                    dist += (val[1] - centroid[channels * k + 1]) * (val[1] - centroid[channels * k + 1]);
+                    dist += (val[2] - centroid[channels * k + 2]) * (val[2] - centroid[channels * k + 2]);
+                    dist = sqrt(dist);
+                    if(dist < min_dist) {
+                        min_dist = dist;
+                        idx = k;
+                    }
+                }
+                // store which cluster the data belong to
+                pt_cluster[j + i * width] = idx;
+            }
+        }
+
+        // clear the value
+        for(int i = 0; i < num_cluster; i++) {
+            num_pt_cluster[i] = 0;
+            sum_dist[0 + i * channels] = 0;
+            sum_dist[1 + i * channels] = 0;
+            sum_dist[2 + i * channels] = 0;
+        }
+
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                idx = pt_cluster[j + i * width]; // get cluster id
+                num_pt_cluster[idx] += 1;
+                sum_dist[idx * channels + 0] += image_src[channels * (j + i * width) + 0];
+                sum_dist[idx * channels + 1] += image_src[channels * (j + i * width) + 1];
+                sum_dist[idx * channels + 2] += image_src[channels * (j + i * width) + 2];
+            }
+        }
+
+        for(int i = 0; i < num_cluster; i++) {
+            if(num_pt_cluster[i] != 0) {
+                new_centroid[channels * i + 0] = sum_dist[i * channels + 0] / num_pt_cluster[i];
+                new_centroid[channels * i + 1] = sum_dist[i * channels + 1] / num_pt_cluster[i];
+                new_centroid[channels * i + 2] = sum_dist[i * channels + 2] / num_pt_cluster[i];
+            }
+        }
+
+        // calculate the sum of difference between old center and new center
+        // Also, save the centroid value
+        sum_val = 0;
+        for(int i = 0; i < num_cluster; i++) {
+            dist = 0;
+            dist += (new_centroid[channels * i + 0] - centroid[channels * i + 0]) * (new_centroid[channels * i + 0] - centroid[channels * i + 0]);
+            dist += (new_centroid[channels * i + 1] - centroid[channels * i + 1]) * (new_centroid[channels * i + 1] - centroid[channels * i + 1]);
+            dist += (new_centroid[channels * i + 2] - centroid[channels * i + 2]) * (new_centroid[channels * i + 2] - centroid[channels * i + 2]);
+            sum_val += sqrt(dist);
+            
+            centroid[channels * i + 0] = new_centroid[channels * i + 0];
+            centroid[channels * i + 1] = new_centroid[channels * i + 1];
+            centroid[channels * i + 2] = new_centroid[channels * i + 2];
+        }
+
+        // if the sum < threshold, stop the iteraton
+        if(sum_val < num_cluster * 6.88) {
+            break;
+        }
+    }
+
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+            val[0] = image_src[channels * (j + i * width) + 0];
+            val[1] = image_src[channels * (j + i * width) + 1];
+            val[2] = image_src[channels * (j + i * width) + 2];
+            min_dist = 1000000;
+        
+            for(int k = 0; k < num_cluster; k++) {
+                // calculate l2 norm
+                dist += (val[0] - centroid[channels * k + 0]) * (val[0] - centroid[channels * k + 0]);
+                dist += (val[1] - centroid[channels * k + 1]) * (val[1] - centroid[channels * k + 1]);
+                dist += (val[2] - centroid[channels * k + 2]) * (val[2] - centroid[channels * k + 2]);
+                dist = sqrt(dist);
+                if(dist < min_dist) {
+                    min_dist = dist;
+                    idx = k;
+                }
+            }
+
+            image_result[channels * (j + i * width) + 0] = centroid[channels * idx + 0];
+            image_result[channels * (j + i * width) + 1] = centroid[channels * idx + 1];
+            image_result[channels * (j + i * width) + 2] = centroid[channels * idx + 2];
+        }
+    }
+}
+
 int main(int argc, char** argv) {
 
     assert(argc == 3);
@@ -85,13 +203,7 @@ int main(int argc, char** argv) {
     read_png(argv[1], &image_src, &height, &width, &channels);
     image_result = (unsigned char*) malloc(height * width * channels * sizeof(unsigned char));
 
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-          image_result[channels * (j + i * width) + 0] = image_src[channels * (j + i * width) + 0];
-          image_result[channels * (j + i * width) + 1] = image_src[channels * (j + i * width) + 1];
-          image_result[channels * (j + i * width) + 2] = image_src[channels * (j + i * width) + 2];
-        }
-    }
+    kmeans(image_src, image_result, height, width, channels, 5);
 
     write_png(argv[2], image_result, height, width, channels);
 
